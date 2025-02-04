ada07aa750643588a4815fea1e8134922715e370
Dmitriy-Daniluk <danilyuk.d.i.23@gmail.com>
2025-02-04 12:20:04 +0300

    task-11

--- src/App.js
+++ src/App.js
@@ -4,32 +4,7 @@ import './App.css';
 import React from 'react';
 
 function App() {
-	return(
-		<>
-			<ul>
-				<li>text1</li>
-				<li>text2</li>
-				<li>text3</li>
-			</ul>
-			<ul>
-				<li>text1</li>
-				<li>text2</li>
-				<li>text3</li>
-			</ul>
-		</>
-		// <div>
-		// 	<ul>
-		// 		<li>text1</li>
-		// 		<li>text2</li>
-		// 		<li>text3</li>
-		// 	</ul>
-		// 	<ul>
-		// 		<li>text1</li>
-		// 		<li>text2</li>
-		// 		<li>text3</li>
-		// 	</ul>
-		// </div>
-	);
+	return <input />;
 }
 
 export default App;
