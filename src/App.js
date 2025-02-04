51669a95897537394ba5ee7d2d5e7a89242ed20c
Dmitriy-Daniluk <danilyuk.d.i.23@gmail.com>
2025-02-04 12:28:33 +0300

    task-16

--- src/App.js
+++ src/App.js
@@ -4,19 +4,17 @@ import './App.css';
 import React from 'react';
 
 function App() {
-	const arr = [1, 2, 3, 4, 5];
+	const obj = {name: 'john', surname: 'smit'};
 	
 	return (
 		<>
-		<ul>
-			<li>{arr[0]}</li>
-			<li>{arr[1]}</li>
-			<li>{arr[2]}</li>
-			<li>{arr[3]}</li>
-			<li>{arr[4]}</li>
-		</ul>
+		<p>
+			name: <span>{obj.name}</span>
+			<br></br>
+			surname: <span>{obj.surname}</span>
+		</p>
 		</>
-	)
+	);
 }
 
 export default App;
