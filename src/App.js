c7e78857b16f1ab7dd3ff07845a9d278e73b7a4c
Dmitriy-Daniluk <danilyuk.d.i.23@gmail.com>
2025-02-04 12:15:03 +0300

    task-10

--- src/App.js
+++ src/App.js
@@ -5,18 +5,30 @@ import React from 'react';
 
 function App() {
 	return(
-		<ul>
-			<li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio eos, consequuntur itaque ipsum vero veniam magnam libero sequi obcaecati cumque dolores iusto recusandae voluptas quas rem perferendis. Velit, eligendi sed!</li>
-			<li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio eos, consequuntur itaque ipsum vero veniam magnam libero sequi obcaecati cumque dolores iusto recusandae voluptas quas rem perferendis. Velit, eligendi sed!</li>
-			<li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio eos, consequuntur itaque ipsum vero veniam magnam libero sequi obcaecati cumque dolores iusto recusandae voluptas quas rem perferendis. Velit, eligendi sed!</li>
-			<li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio eos, consequuntur itaque ipsum vero veniam magnam libero sequi obcaecati cumque dolores iusto recusandae voluptas quas rem perferendis. Velit, eligendi sed!</li>
-			<li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio eos, consequuntur itaque ipsum vero veniam magnam libero sequi obcaecati cumque dolores iusto recusandae voluptas quas rem perferendis. Velit, eligendi sed!</li>
-			<li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio eos, consequuntur itaque ipsum vero veniam magnam libero sequi obcaecati cumque dolores iusto recusandae voluptas quas rem perferendis. Velit, eligendi sed!</li>
-			<li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio eos, consequuntur itaque ipsum vero veniam magnam libero sequi obcaecati cumque dolores iusto recusandae voluptas quas rem perferendis. Velit, eligendi sed!</li>
-			<li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio eos, consequuntur itaque ipsum vero veniam magnam libero sequi obcaecati cumque dolores iusto recusandae voluptas quas rem perferendis. Velit, eligendi sed!</li>
-			<li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio eos, consequuntur itaque ipsum vero veniam magnam libero sequi obcaecati cumque dolores iusto recusandae voluptas quas rem perferendis. Velit, eligendi sed!</li>
-			<li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio eos, consequuntur itaque ipsum vero veniam magnam libero sequi obcaecati cumque dolores iusto recusandae voluptas quas rem perferendis. Velit, eligendi sed!</li>
-		</ul>
+		<>
+			<ul>
+				<li>text1</li>
+				<li>text2</li>
+				<li>text3</li>
+			</ul>
+			<ul>
+				<li>text1</li>
+				<li>text2</li>
+				<li>text3</li>
+			</ul>
+		</>
+		// <div>
+		// 	<ul>
+		// 		<li>text1</li>
+		// 		<li>text2</li>
+		// 		<li>text3</li>
+		// 	</ul>
+		// 	<ul>
+		// 		<li>text1</li>
+		// 		<li>text2</li>
+		// 		<li>text3</li>
+		// 	</ul>
+		// </div>
 	);
 }
 
