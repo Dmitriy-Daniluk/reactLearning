014af692f29b5158b39c5450b30a605375076687
Dmitriy-Daniluk <danilyuk.d.i.23@gmail.com>
2025-02-04 12:41:37 +0300

    task-23

--- src/App.js
+++ src/App.js
@@ -4,14 +4,47 @@ import './App.css';
 import React from 'react';
 
 function App() {
-	return <div>
-		text
-		<br />
-		<input />
-		<br />
-		<input />
-		<br />
-		<input />
-	</div>;
+	return (
+		<>
+		<div>
+			<h1>№1</h1>
+			<ul>
+				<li>text</li>
+				<li>text</li>
+				<li>text</li>
+				<li>text</li>
+				<li>text</li>
+				<li>text</li>
+				<li>text</li>
+				<li>text</li>
+				<li>text</li>
+				<li>text</li>
+			</ul>
+		</div>
+
+		<div>
+			<h1>№2</h1>
+			<table>
+				<tbody>
+					<tr>
+						<td>1</td>
+						<td>2</td>
+						<td>3</td>
+					</tr>
+					<tr>
+						<td>4</td>
+						<td>5</td>
+						<td>6</td>
+					</tr>
+					<tr>
+						<td>7</td>
+						<td>8</td>
+						<td>9</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+		</>
+	);
 }
 export default App;
