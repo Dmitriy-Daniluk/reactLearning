ee13c798ed754818f28613dc10369c9bd26c528d
Dmitriy-Daniluk <danilyuk.d.i.23@gmail.com>
2025-03-10 09:26:49 +0300

    task 34

--- src/App.js
+++ src/App.js
@@ -5,17 +5,22 @@ import React from 'react';
 
 
 function App() {
-	function show1() {
-		alert(1);
+	function msg1() {
+		alert(1)
+	}
+
+	function msg2() {
+		alert(2)
 	}
 	
-	function show2() {
-		alert(2);
+	function msg3() {
+		alert(3)
 	}
-	
+
 	return <div>
-		<button onClick={show1}>act1</button>
-		<button onClick={show2}>act2</button>
+		<button onClick={msg1}>act1</button>
+		<button onClick={msg2}>act2</button>
+		<button onClick={msg3}>act3</button>
 	</div>;
 }
 
