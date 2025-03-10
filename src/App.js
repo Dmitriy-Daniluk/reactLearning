0f43a345f9502e5295fdeb88c27b61253db6aa70
Dmitriy-Daniluk <danilyuk.d.i.23@gmail.com>
2025-03-10 09:28:10 +0300

    task 35

--- src/App.js
+++ src/App.js
@@ -5,22 +5,14 @@ import React from 'react';
 
 
 function App() {
-	function msg1() {
-		alert(1)
-	}
-
-	function msg2() {
-		alert(2)
-	}
-	
-	function msg3() {
-		alert(3)
+	function showMess(text) {
+		alert(text);
 	}
 
 	return <div>
-		<button onClick={msg1}>act1</button>
-		<button onClick={msg2}>act2</button>
-		<button onClick={msg3}>act3</button>
+		<button onClick={() => showMess('1')}>act1</button>
+		<button onClick={() => showMess('2')}>act2</button>
+		<button onClick={() => showMess('3')}>act3</button>
 	</div>;
 }
 
