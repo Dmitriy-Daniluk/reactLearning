a12b288dba0da20f044b306c1d02d5f18bbe49f8
Dmitriy-Daniluk <danilyuk.d.i.23@gmail.com>
2025-04-01 12:32:19 +0300

    task 49

--- src/App.js
+++ src/App.js
@@ -9,15 +9,22 @@ function id() {
 
 function App() 
 {
-  const uniqueId = id();
+	const prods = [
+		{id: id(), name: 'product1', cost: 100},
+		{id: id(), name: 'product2', cost: 200},
+		{id: id(), name: 'product3', cost: 300},
+	];
+	
+	const res = prods.map(function(item) {
+			return <p key={item.id}>{item.name}: {item.cost}</p>;
+	});	
 
   return (
     <div>
-      Сгенерированный ID: {uniqueId}
+      {res}
     </div>
   );
 }
-
 
 
 export default App;
