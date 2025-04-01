a38738ff397f7f284d5d5a75c661325736e74b66
Dmitriy-Daniluk <danilyuk.d.i.23@gmail.com>
2025-04-01 12:35:58 +0300

    task 53

--- src/App.js
+++ src/App.js
@@ -9,15 +9,15 @@ import { useState } from 'react';
 function App() 
 {
 	const [name_user, setName1] = useState('Дмитрий');
-	const [group, setName2] = useState('И-2-23');
-	const [age, setName3] = useState('18');
+	const [surname_user, setName2] = useState('Данилюк');
 	const changeName = () => {
 		setName1('Измененное имя');
 	};
   return (
     <div>
-      Имя пользователя: {name_user}, группа: {group}, его возраст: {age} <br />
-	  <button onClick={changeName}>Изменить имя</button>
+      Имя пользователя: {name_user} {surname_user}<br />
+			<button onClick={changeName}>Изменить имя</button>
+			<button onClick={() => setName2('Измененная фамилия')}>Изменить фамилию</button>
     </div>
   );
 }
