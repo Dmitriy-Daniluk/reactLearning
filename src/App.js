36fe96122f95c79dcf6026172fa17bbb19c776c6
Dmitriy-Daniluk <danilyuk.d.i.23@gmail.com>
2025-04-01 12:07:40 +0300

    task 37

--- src/App.js
+++ src/App.js
@@ -5,33 +5,17 @@ import React from 'react';
 
 
 function App() {
-	function func1(arg, event) {
-    console.log(arg, event);
-  }
+	const arr = [
+		<li>1</li>,
+		<li>2</li>,
+		<li>3</li>,
+		<li>4</li>,
+		<li>5</li>,
+	];
 
-  function func2(arg1, arg2, event) {
-    console.log(arg1, arg2, event);
-  }
-
-  function func3(event, arg) {
-    console.log(event, arg);
-  }
-
-  function func4(arg1, event, arg2) {
-    console.log(arg1, event, arg2);
-  }
-
-	return (
-		<div>
-      <button onClick={event => func1('eee', event)}>act 1</button>
-
-      <button onClick={event => func2('eee', event)}>act 2</button>
-
-      <button onClick={event => func3(event, 'eee')}>act 3</button>
-
-      <button onClick={event => func4('arg1', event, 'arg2')}>act 4</button>
-		</div>	
-	);
+	return <div>
+		{arr}
+	</div>
 }
 
 
