5eb71785915e1a9d5a28eb4bce877726b82a6d7f
Dmitriy-Daniluk <danilyuk.d.i.23@gmail.com>
2025-04-01 12:19:46 +0300

    task 41

--- src/App.js
+++ src/App.js
@@ -6,15 +6,21 @@ import React from 'react';
 
 
 function App() {
-	const arr = ['a', 'b', 'c', 'd', 'e'];
-	
-	const res = arr.map(function(item, index) {
-		return <li key={index}>{item}</li>;
+	const names = [
+		{name: 'name1', surn: 'surn1', age: 30},
+		{name: 'name2', surn: 'surn2', age: 31},
+		{name: 'name3', surn: 'surn3', age: 32},
+	];
+
+	const res = names.map(function(item, index) {
+		return <ul><p key={index}>
+			<li>Имя пользователя: {item.name}, фамилия пользователя: {item.surn}, возраст пользователя: {item.age}</li>
+		</p>
+		</ul>;
 	});
-	
 	return <div>
 		<ul>
-		{res}
+			{res}
 		</ul>
 	</div>;
 
