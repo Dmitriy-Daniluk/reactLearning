fb06722d64cab8d3efe06ff6e959509899066783
Dmitriy-Daniluk <danilyuk.d.i.23@gmail.com>
2025-04-01 12:51:11 +0300

    task 64

--- src/App.js
+++ src/App.js
@@ -6,40 +6,22 @@ import { useState } from 'react';
 
 
 function App() {
-	const [checked, setChecked] = useState(true);
-	let message;
-	const [checked2, setChecked2] = useState(true);
-	let message2;
-
-	if (checked2) {
-		message2 = <p>Новый текст</p>
+	const [value, setValue] = useState('');
+	
+	function handleChange(event) {
+		setValue(event.target.value);
 	}
-
-	if (checked) {
-		message = <div>
-			<h2>Вам 18 и больше</h2>
-			<p>только если больше 18 лет</p>
-		</div>;
-	} else {
-		message = <div>
-			<p>слишком молод</p>
-		</div>;
-	}
-
-
+	
 	return <div>
-		<div>
-			Задание 1: <br />
-			Подтвердите, что вам есть 18 лет:
-			<input type="checkbox" checked={checked} onChange={() => setChecked(!checked)} />
-			<div>{message}</div>
-		</div>
-		<div>
-			Задание 2: <br />
-			Попробуйте нажать на галочку:
-			<input type="checkbox" checked={checked2} onChange={() => setChecked2(!checked2)} />
-			<div>{message2}</div>
-		</div>
+		<select value={value} onChange={handleChange}>
+			<option>Симферополь</option>
+			<option>Не Симферополь</option>
+			<option>Москва</option>
+			<option>Не Москва</option>
+		</select>
+		<p>
+			ваш город: {value}
+		</p>
 	</div>;
 }
 
