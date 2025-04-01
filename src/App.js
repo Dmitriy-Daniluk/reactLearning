8efd9805f6257d10bd3e86bd3cfc5dfe0d4277f7
Dmitriy-Daniluk <danilyuk.d.i.23@gmail.com>
2025-04-01 12:06:55 +0300

    task 35

--- src/App.js
+++ src/App.js
@@ -5,15 +5,32 @@ import React from 'react';
 
 
 function App() {
-	function handleClick(event) {
-		console.log(event);
-		console.log(event.target);
-	}
+	function func1(arg, event) {
+    console.log(arg, event);
+  }
+
+  function func2(arg1, arg2, event) {
+    console.log(arg1, arg2, event);
+  }
+
+  function func3(event, arg) {
+    console.log(event, arg);
+  }
+
+  function func4(arg1, event, arg2) {
+    console.log(arg1, event, arg2);
+  }
 
 	return (
 		<div>
-			<button onClick={handleClick}>act</button>
-		</div>
+      <button onClick={event => func1('eee', event)}>act 1</button>
+
+      <button onClick={event => func2('eee', event)}>act 2</button>
+
+      <button onClick={event => func3(event, 'eee')}>act 3</button>
+
+      <button onClick={event => func4('arg1', event, 'arg2')}>act 4</button>
+		</div>	
 	);
 }
 
