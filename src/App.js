eef6512144859cd1ac587d6029da675663144a3c
Dmitriy-Daniluk <danilyuk.d.i.23@gmail.com>
2025-04-01 12:30:59 +0300

    task 48

--- src/App.js
+++ src/App.js
@@ -1,29 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 
-import { nanoid } from 'nanoid';
-import { v4 as uuidv4 } from 'uuid';
+import { nanoid } from 'nanoid'; 
+	
+function id() {
+  return nanoid(); 
+}
 
 function App() 
 {
-  const randomIdsNanoid = Array.from({ length: 5 }, () => nanoid());
-  const randomIdsUUID = Array.from({ length: 5 }, () => uuidv4());
+  const uniqueId = id();
 
   return (
     <div>
-      <h2>Случайные строки библиотеки nanoid:</h2>
-      <ul>
-        {randomIdsNanoid.map(id => (
-          <li key={id}>{id}</li>
-        ))}
-      </ul>
-
-      <h2>Случайные строки библиотеки uuid:</h2>
-      <ul>
-        {randomIdsUUID.map(id => (
-          <li key={id}>{id}</li>
-        ))}
-      </ul>
+      Сгенерированный ID: {uniqueId}
     </div>
   );
 }
