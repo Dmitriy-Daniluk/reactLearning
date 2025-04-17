7dfe5904c865adf573d47b475e5a189d580fffba
Dmitriy-Daniluk <danilyuk.d.i.23@gmail.com>
2025-04-17 08:29:22 +0300

    task 85

--- src/App.js
+++ src/App.js
@@ -2,38 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 
 import React, { useState } from 'react';
+import Users from './Users'; // Импортируем компонент Users
+// import Users from './Users'; // Импортируем компонент Users
 import Product from './Product'; // импортируем продукт
-import User from './User'; // импортируем user
 import Employee from './Employee'; // Импортируем компонент Employee
-
-const users = [
-	{ id: 1, name: 'user1', surn: 'surn1', age: 30 },
-	{ id: 2, name: 'user2', surn: 'surn2', age: 31 },
-	{ id: 3, name: 'user3', surn: 'surn3', age: 32 },
-];
 
 function App() {
 	return (
 			<div>
-					<table>
-							<thead>
-									<tr>
-											<th>Имя</th>
-											<th>Фамилия</th>
-											<th>Возраст</th>
-									</tr>
-							</thead>
-							<tbody>
-							{users.map(user => (
-								<User 
-									key={user.id} // Добавляем уникальный ключ для каждого компонента
-									name={user.name} 
-									surn={user.surn} 
-									age={user.age} 
-								/>
-							))}
-							</tbody>
-					</table>
+					<h1>Список пользователей</h1>
+					<Users /> 
 			</div>
 	);
 }
