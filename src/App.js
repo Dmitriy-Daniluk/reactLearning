e0d6e2c0fc7f4d11a3b8dbe588d663ff653385d3
Dmitriy-Daniluk <danilyuk.d.i.23@gmail.com>
2025-04-17 08:18:39 +0300

    task 81

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import './App.css';
 import React, { useState } from 'react';
 import Product from './Product'; // импортируем продукт
 import User from './User'; // импортируем user
+import Employee from './Employee'; // Импортируем компонент Employee
 
 function App() {
-	return <div>
-		<User />
-		<User />
-		<User />
-		<User />
-	</div>;
+    return (
+        <div>
+            <Employee lastName="Данилюк" firstName="Дмитрий" middleName="Витальевич" salary="1" />
+            <Employee lastName="Киселев" firstName="Максим" middleName="Вадимович" salary="999999" />
+            <Employee lastName="Иванов" firstName="Иван" middleName="Иванович" salary="100000" />
+        </div>
+    );
 }
 
 export default App;
