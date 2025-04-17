34d8981b5c8c11e3f0ae944885b0141794eb9fd3
Dmitriy-Daniluk <danilyuk.d.i.23@gmail.com>
2025-04-17 07:59:13 +0300

    task 67

--- src/App.js
+++ src/App.js
@@ -1,29 +1,88 @@
 import logo from './logo.svg';
 import './App.css';
 
-import { useState } from 'react';
-
-
+import React, { useState } from 'react';
 
 function App() {
-	const [value, setValue] = useState('');
+	const [value, setValue] = useState(1);
 	
+	function changeHandler(event) {
+		setValue(event.target.value);
+	}
+
+  const [language, setLanguage] = useState('');
+
+  function changeHandler2(event) {
+    setLanguage(event.target.value);
+  }
 	
 	return <div>
-		<select value={value} onChange={event => setValue(event.target.value)}>
-			<option value="1">мне 0 - 12 лет</option>
-			<option value="2">мне 13 - 17 лет</option>
-			<option value="3">мне 18 - 25 лет</option>
-			<option value="4">мне больше 25 лет</option>
-		</select>
-		<p>
-			{value === '1' && 'вы выбрали 0 - 12 лет'}
-			{value === '2' && 'вы выбрали 13 - 17 лет'}
-			{value === '3' && 'вы выбрали 18 - 25 лет'}
-			{value === '4' && 'вы выбрали больше 25 лет'}
-		</p>
-	</div>;
-}
+    Задание 1:
+    <div>
+		<input
+			type="radio"
+			name="radio"
+			value="1"
+			checked={value === '1' ? true : false}
+			onChange={changeHandler}
+		/>
+		<input
+			type="radio"
+			name="radio"
+			value="2"
+			checked={value === '2' ? true : false}
+			onChange={changeHandler}
+		/>
+		<input
+			type="radio"
+			name="radio"
+			value="3"
+			checked={value === '3' ? true : false}
+			onChange={changeHandler}
+		/>
 
+    <p>Вы выбрали: {value}</p>
+    </div>
+    <div>
+      Задание 2:
+    <div>
+      <h2>Какой ваш любимый язык программирования?</h2>
+      
+      <input
+        type="radio"
+        name="language"
+        value="JavaScript"
+        checked={language === 'JavaScript'}
+        onChange={changeHandler2}
+      />
+      <label>JavaScript</label>
+      
+      <input
+        type="radio"
+        name="language"
+        value="Python"
+        checked={language === 'Python'}
+        onChange={changeHandler2}
+      />
+      <label>Python</label>
+      
+      <input
+        type="radio"
+        name="language"
+        value="Java"
+        checked={language === 'Java'}
+        onChange={changeHandler2}
+      />
+      <label>Java</label>
+
+      <p>
+        Ваш любимый язык программирования: {language}
+      </p>
+
+      {language === 'JavaScript' && <p>О, вы выбрали: JavaScript. JavaScript — это замечательный язык программирования</p>}
+    </div>
+    </div>
+	</div>
+}
 
 export default App;
