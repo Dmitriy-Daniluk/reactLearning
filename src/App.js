ad0acf0b8d3e0cfa8708929da980c6d6a3088f61
Dmitriy-Daniluk <danilyuk.d.i.23@gmail.com>
2025-04-17 08:02:46 +0300

    task 71

--- src/App.js
+++ src/App.js
@@ -3,23 +3,51 @@ import './App.css';
 
 import React, { useState } from 'react';
 
-function App() {
-  const [checked, setChecked] = useState(true);
-  const [value, setValue] = useState('Начальное значение');
+const initDate = {
+  year: 2025,
+  month: 12,
+  day: 31,
+};
 
+const daysOfWeek = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+
+function App() {
+  const [date, setDate] = useState(initDate);
+
+  function handleChange(prop, event) {
+    setDate({ ...date, [prop]: event.target.value });
+  }
+
+  const currentDate = new Date(date.year, date.month - 1, date.day);
+  const dayOfWeekIndex = currentDate.getDay(); 
+  const dayOfWeek = daysOfWeek[dayOfWeekIndex]; 
 
   return (
     <div>
-    <div>
-      <h2>Чекбокс с начальным состоянием</h2>
-      <input type="checkbox" defaultChecked={!checked} />
-      <label>Чекбокс по умолчанию отмечен</label>
+      <input 
+        type="number" 
+        value={date.year} 
+        onChange={event => handleChange('year', event)} 
+        placeholder="Год" 
+      />
+      <input 
+        type="number" 
+        value={date.month} 
+        onChange={event => handleChange('month', event)} 
+        placeholder="Месяц" 
+      />
+      <input 
+        type="number" 
+        value={date.day} 
+        onChange={event => handleChange('day', event)} 
+        placeholder="День" 
+      />
+
+      <br />
+      <p>
+        {date.year}-{date.month}-{date.day} ({dayOfWeek})
+      </p>
     </div>
-    <div>
-    <h2>Текстовое поле с начальным значением</h2>
-    <input defaultValue={value} />
-  </div>
-  </div>
   );
 }
 
