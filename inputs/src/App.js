c4b4430525518d5286e58040fa62710efe9157d5
Dmitriy-Daniluk <danilyuk.d.i.23@gmail.com>
2025-05-12 23:29:46 +0300

    task 108

--- inputs/src/App.js
+++ inputs/src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import classes from "./App.module.css"; 
+import Inputs from "./components/Inputs"; 
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div class={classes.app}>
+			<h2 class={classes.title}>CSS Module Inputs</h2>
+			<div class={classes.wrapper}>
+				<Inputs />
+			</div>
+		</div>
+	);
 }
-
 export default App;
