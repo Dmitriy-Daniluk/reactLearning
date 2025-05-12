4fade60f26e65b807819c552ff9eb5ee545cc0f0
Dmitriy-Daniluk <danilyuk.d.i.23@gmail.com>
2025-05-12 22:46:58 +0300

    task 88

--- src/Users.js
+++ src/Users.js
@@ -1,27 +1,31 @@
 import React, { useState } from 'react';
-import User from './User'; 
-
+import User from './User';
 
 const Users = () => {
 	const [users, setUsers] = useState([
-			{ id: 1, name: 'John John', email: 'john@john.com' },
-			{ id: 2, name: 'ABC ABC', email: 'ABC@ABC.com' },
-			{ id: 3, name: 'BBB BBB', email: 'BBB@example.com' },
+		{ id: 1, name: 'John 1', email: 'john1@example.com' },
+		{ id: 2, name: 'John 2', email: 'john1@example.com' },
+		{ id: 3, name: 'John 3', email: 'john1@example.com' },
 	]);
 
 	const banUser = (id) => {
-			setUsers(prevUsers => prevUsers.filter(user => user.id !== id));
-			console.log('Пользователь с ID ',id ,' забанен');
+		setUsers(prevUsers => prevUsers.filter(user => user.id !== id));
+		console.log('Пользователь с ID ', id, ' забанен');
+	};
+
+	const editUser = (id, updatedUser) => {
+		setUsers(prevUsers => prevUsers.map(user =>
+			user.id === id ? { ...user, ...updatedUser } : user
+		));
 	};
 
 	return (
-			<div>
-					{users.map(user => (
-							<User key={user.id} user={user} onBan={banUser} /> 
-					))}
-			</div>
+		<div>
+			{users.map(user => (
+				<User key={user.id} user={user} onBan={banUser} onEdit={editUser} />
+			))}
+		</div>
 	);
 };
-
 
 export default Users;
