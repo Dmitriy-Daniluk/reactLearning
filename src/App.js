f23109881ce5c3e0f6e97fb53b7055aae7711917
Dmitriy-Daniluk <danilyuk.d.i.23@gmail.com>
2025-05-12 22:48:54 +0300

    task 89

--- src/App.js
+++ src/App.js
@@ -7,14 +7,42 @@ import Users from './Users'; // Импортируем компонент Users
 import Product from './Product'; // импортируем продукт
 import Employee from './Employee'; // Импортируем компонент Employee
 
-function App() {
+const App = () => {
+	const [users, setUsers] = useState([
+		{ id: 1, name: 'John 1', email: 'john1@example.com' },
+		{ id: 2, name: 'John 2', email: 'john1@example.com' },
+		{ id: 3, name: 'John 3', email: 'john1@example.com' },
+	]);
+
+	const banUser = (id) => {
+		setUsers(prevUsers => prevUsers.filter(user => user.id !== id));
+		console.log('Пользователь с ID ', id, ' забанен');
+	};
+
+	const editUser = (id, updatedFields) => {
+		setUsers(prevUsers => prevUsers.map(user =>
+			user.id === id ? { ...user, ...updatedFields } : user
+		));
+	};
+
+	const UserField = ({ id, text, type, isEdit, editUser }) => {
+		return isEdit ? (
+			<input
+				value={text}
+				onChange={event => editUser(id, type, event.target.value)}
+			/>
+		) : (
+			<span>{text}</span>
+		);
+	};
+
 	return (
-			<div>
-					<h1>Список пользователей</h1>
-					<Users /> 
-			</div>
+		<div>
+			<h1>Список пользователей</h1>
+			<Users users={users} onBan={banUser} onEdit={editUser} UserField={UserField} />
+		</div>
 	);
-}
+};
 
 
 export default App;
--- src/Users.js
+++ src/Users.js
@@ -1,28 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import User from './User';
 
-const Users = () => {
-	const [users, setUsers] = useState([
-		{ id: 1, name: 'John 1', email: 'john1@example.com' },
-		{ id: 2, name: 'John 2', email: 'john1@example.com' },
-		{ id: 3, name: 'John 3', email: 'john1@example.com' },
-	]);
-
-	const banUser = (id) => {
-		setUsers(prevUsers => prevUsers.filter(user => user.id !== id));
-		console.log('Пользователь с ID ', id, ' забанен');
-	};
-
-	const editUser = (id, updatedUser) => {
-		setUsers(prevUsers => prevUsers.map(user =>
-			user.id === id ? { ...user, ...updatedUser } : user
-		));
-	};
-
+const Users = ({ users, onBan, onEdit, UserField }) => {
 	return (
 		<div>
 			{users.map(user => (
-				<User key={user.id} user={user} onBan={banUser} onEdit={editUser} />
+				<User key={user.id} user={user} onBan={onBan} onEdit={onEdit} UserField={UserField} />
 			))}
 		</div>
 	);
