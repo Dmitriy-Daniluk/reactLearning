59480aaf86afb211095280ed1f323309d8ecc92e
Dmitriy-Daniluk <danilyuk.d.i.23@gmail.com>
2025-05-12 23:21:16 +0300

    task 108

--- buttons/src/App.js
+++ buttons/src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import classes from "./App.module.css";
+import Buttons from "./components/Buttons";
 
 function App() {
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
+		<div class={classes.app}>
+			<h2 class={classes.title}>CSS Module Buttons</h2>
+			<div class={classes.wrapper}>
+				<Buttons />
+			</div>
 		</div>
 	);
 }
-
 export default App;
